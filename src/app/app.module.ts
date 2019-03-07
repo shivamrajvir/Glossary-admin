83ce3ed99a601f08a43f9c7db17b2adec381eeb9
Shivam <shivamrajvir@gmail.com>
2019-03-07 21:56:01 +0530

    Login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import {MAT_LABEL_GLOBAL_OPTIONS, MatCardModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MaterialModules} from './material-modules';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModules,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_LABEL_GLOBAL_OPTIONS, useValue: {float: 'always'}},
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
