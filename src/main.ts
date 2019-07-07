9d437dea143c2c92cf8b5ab7190622e8305090e2
Shivam Rajvir <shivamrajvir@gmail.com>
2019-07-07 13:37:38 +0530

    console log and error removed from main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  if (window) {
+    window.console.log = function () { };
+    window.console.error = function () { };
+  }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
